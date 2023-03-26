d55f72c7cceaa2ae331397f71e5587269c1797f7
Christian <111076892+i-christian@users.noreply.github.com>
2023-03-27 00:41:15 +0200

    done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Client SSH configuration file
+
+file { '/home/christian/.ssh/config':
+  ensure  => file,
+  owner   => 'christian',
+  group   => 'christian',
+  mode    => '0600',
+  content => "
+Host 3.80.19.9
+IdentityFile ~/.ssh/school
+PasswordAuthentication no
+ "
+}
