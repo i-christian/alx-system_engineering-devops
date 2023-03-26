3f2e95656637561f065c97b91e47688acc3f13cc
Christian <111076892+i-christian@users.noreply.github.com>
2023-03-27 00:57:00 +0200

    update advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Client SSH configuration file
 
-file { '~/.ssh/config':
+file { '/home/christian/.ssh/config':
   ensure  => file,
   owner   => 'christian',
   group   => 'christian',
   mode    => '0600',
   content => "
 Host 3.80.19.9
-IdentityFile ~/.ssh/school
-PasswordAuthentication no
- "
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+ ",
 }
