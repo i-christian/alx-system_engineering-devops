a48e077e250aeaa248215ef7b1aa6610af61820e
Christian <111076892+i-christian@users.noreply.github.com>
2023-04-05 02:40:45 +0200

    puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Set up Nginx
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
+
+# Configure custom HTTP header
+class custom_header {
+  file { '/etc/nginx/conf.d/custom_headers.conf':
+    ensure  => present,
+    content => "add_header X-Served-By $::hostname always;",
+    notify  => Class['nginx'],
+  }
+}
+
+# Apply classes
+include nginx
+include custom_header
