a77fb22d96725faaff795ff8ac91e18ff93293f6
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-18 00:01:41 +0200

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -11,7 +12,7 @@ def number_of_subscribers(subreddit):
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        response.raise_for_status()
 
         if response.status_code == 200:
             results = response.json().get("data")
